1e673caf17e7ae9888b405e8371256f776a5361d
Linus Learnuel <63509139+Learnuel1@users.noreply.github.com>
2024-12-03 17:36:52 +0100

    feat: update ui with login and sign functions

--- src/api/noteapi.js
+++ src/api/noteapi.js
@@ -1,5 +1,5 @@
-// const baseUrl = "http://localhost:5001/api/v1";
-const baseUrl= "https://note-manage-backend.onrender.com/api/v1";
+const baseUrl = "http://localhost:5001/api/v1";
+// const baseUrl= "https://note-manage-backend.onrender.com/api/v1";
 
 const header = {
   "content-type": "application/json",
